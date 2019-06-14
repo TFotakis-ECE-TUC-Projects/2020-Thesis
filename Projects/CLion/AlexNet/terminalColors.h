6bc09a2d50e2a3fd00cd4f8c8ca211322e796678
TFotakis <fotakistzanis@gmail.com>
2019-06-14 18:37:20 +0300

    Added documentation to terminalColors.h

--- Projects/CLion/AlexNet/terminalColors.h
+++ Projects/CLion/AlexNet/terminalColors.h
@@ -1,3 +1,13 @@
+// Contains definitions of terminal color characters
+// Use by printing the color character before the
+// string to be colored. Do not forget to reset the
+// buffer back to normal colors by using the KNRM
+// character.
+//
+// Example:
+// printf("%sThis is a red string! %sAnd this is a normal one.", KRED, KNRM)
+
+
 #define KNRM  "\x1B[0m"
 #define KRED  "\x1B[31m"
 #define KGRN  "\x1B[32m"
