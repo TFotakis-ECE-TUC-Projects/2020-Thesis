3faed5d7939bcfac5920246884cb57be0b011d90
TFotakis <fotakistzanis@gmail.com>
2019-06-16 23:50:20 +0300

    Added some documentation on nn-tools.h

--- Projects/CLion/AlexNet/nn-tools.h
+++ Projects/CLion/AlexNet/nn-tools.h
@@ -1,4 +1,20 @@
-FloatMatrix *Conv2d(FloatMatrix *x, FloatMatrix *weights, FloatMatrix *bias, uint in_channels, uint out_channels, uint kernel_size, uint stride, uint padding, char *layerName) {
+/**
+ * Calculates the 2-dimensional convolution of FloatMatrix x and filters stored
+ * in FloatMatrix weights, biased by FloatMatrix bias.
+ * @param[in] x: The input FloatMatrix to convolute
+ * @param[in] weights: The kernels to convolute with
+ * @param[in] bias: The bias of the convolution
+ * @param[in] in_channels: The number of x's channels
+ * @param[in] out_channels: The number of output channels
+ * @param[in] kernel_size: The kernels' dimensions
+ * @param[in] stride: The amount of pixels to skip for each convolution
+ * @param[in] padding: The circular padding applied to the input before convoluting
+ * @param[in] layerName: This layer's name
+ * @returns a FloatMatrix pointer containing the convolution's result
+ */
+FloatMatrix *Conv2d(FloatMatrix *x, FloatMatrix *weights, FloatMatrix *bias,
+		uint in_channels, uint out_channels, uint kernel_size, uint stride,
+		uint padding, char *layerName) {
 	FloatMatrix *tmp = zero3DFloatMatrix(x->dims[0], x->dims[1] + 2 * padding, x->dims[2] + 2 * padding);
 #ifdef _OMP_H
 #pragma omp parallel for
@@ -56,6 +72,13 @@ FloatMatrix *Conv2d(FloatMatrix *x, FloatMatrix *weights, FloatMatrix *bias, uin
 }
 
 
+/**
+ * Applies Rectified Linear Unit on the input FloatMatrix, in other words, it
+ * zeroes out every negative value.
+ * @param[in] x: The input FloatMatrix to activate.
+ * @param[in] layerName: This layer's name
+ * @returns a FloatMatrix pointer containing the activated values
+ */
 FloatMatrix *ReLU(FloatMatrix *x, char *layerName) {
 	uint xLen = flattenDimensions(x);
 	for (int i = 0; i < xLen; i++) {
@@ -72,7 +95,18 @@ FloatMatrix *ReLU(FloatMatrix *x, char *layerName) {
 }
 
 
-FloatMatrix *MaxPool2d(FloatMatrix *x, uint kernel_size, uint stride, char *layerName) {
+/**
+ * Applies 2-dimensional max pooling on the input FloatMatrix.
+ * Creates pools of pixels, finds the maximum value for each pool and assigns
+ * that value to the output FloatMatrix.
+ * @param[in] x: The input FloatMatrix to apply max pooling
+ * @param[in] kernel_size: The kernel's dimensions
+ * @param[in] stride: The amount of pixels to skip for every pool
+ * @param[in] layerName: This layer's name
+ * @returns a FloatMatrix pointer containing the max pooling's result
+ */
+FloatMatrix *MaxPool2d(FloatMatrix *x, uint kernel_size, uint stride,
+		char *layerName) {
 	FloatMatrix *res = zero3DFloatMatrix(x->dims[0], (x->dims[1] - kernel_size) / stride + 1, (x->dims[2] - kernel_size) / stride + 1);
 #ifdef _OMP_H
 #pragma omp parallel for
@@ -109,7 +143,18 @@ FloatMatrix *MaxPool2d(FloatMatrix *x, uint kernel_size, uint stride, char *laye
 }
 
 
-FloatMatrix *Linear(FloatMatrix *x, FloatMatrix *weights, FloatMatrix *bias, uint in_features, uint out_features, char *layerName) {
+/**
+ * Applies a linear transformation to the input FloatMatrix
+ * @param[in] x: The input FloatMatrix
+ * @param[in] weights: The weights to use for the linear transformation
+ * @param[in] bias: The bias to add to the linear transformation
+ * @param[in] in_features: The number of input features (dimension of x)
+ * @param[in] out_features: The number of output features (dimension of the result)
+ * @param[in] layerName: This layer's name
+ * @returns a FloatMatrix pointer containing the linear transformation's result
+ */
+FloatMatrix *Linear(FloatMatrix *x, FloatMatrix *weights, FloatMatrix *bias,
+		uint in_features, uint out_features, char *layerName) {
 	FloatMatrix *res = create1DFloatMatrix(out_features);
 #ifdef _OMP_H
 #pragma omp parallel for
@@ -132,6 +177,12 @@ FloatMatrix *Linear(FloatMatrix *x, FloatMatrix *weights, FloatMatrix *bias, uin
 }
 
 
+/**
+ * Applies the log(SoftMax(x)) function to the input FloatMatrix.
+ * @param[in] x: The input FloatMatrix
+ * @param[in] layerName: This layer's name
+ * @returns a FloatMatrix pointer containing the LogSoftMax's result
+ */
 FloatMatrix *LogSoftMax(FloatMatrix *x, char *layerName) {
 	FloatMatrix *res = create1DFloatMatrix(flattenDimensions(x));
 	matrix_t sum = 0;
