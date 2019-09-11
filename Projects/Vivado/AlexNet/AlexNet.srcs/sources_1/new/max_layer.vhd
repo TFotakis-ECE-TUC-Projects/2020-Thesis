0be7ab013d28447b7e6d34dd61fe0c5cdb926ead
TFotakis <fotakistzanis@gmail.com>
2019-09-11 18:21:56 +0300

    Updated max_layer to use new bus_array

--- Projects/Vivado/AlexNet/AlexNet.srcs/sources_1/new/max_layer.vhd
+++ Projects/Vivado/AlexNet/AlexNet.srcs/sources_1/new/max_layer.vhd
@@ -7,7 +7,7 @@ use work.bus_array_pkg.all;
 entity max_layer is
 	generic	(input_size: positive := 9);
 	Port (
-		Din: in bus_array(input_size - 1 downto 0);
+		Din: in bus_array(input_size - 1 downto 0)(31 downto 0);
 		Dout: out STD_LOGIC_VECTOR (31 downto 0)
 	);
 end max_layer;
@@ -17,36 +17,36 @@ architecture Structural of max_layer is
 	constant modules: positive := integer(floor(real(input_size) / 2.0));
 	constant outputs: positive := integer(ceil(real(input_size) / 2.0));
 
-	signal m: bus_array(outputs - 1 downto 0);
+	signal m: bus_array(outputs - 1 downto 0)(31 downto 0);
 begin
 	Max_mod:
 	for i in 0 to modules - 1 generate
-		max_x: 
-		entity work.max 
+		max_x:
+		entity work.max
 			Port Map(
 				In0 => Din(i * 2),
 				In1 => Din(i * 2 + 1),
 				Dout => m(i)
 			);
 	end generate;
-	
-	
-	Odd_Input: 
+
+
+	Odd_Input:
 	if input_size mod 2 = 1 generate
 		m(outputs - 1) <= Din(input_size - 1);
 	end generate;
-	
-	
-	Next_Layer: 
+
+
+	Next_Layer:
 	if input_size /= 2 generate
-		next_max_layer: 
-		entity work.max_layer 
+		next_max_layer:
+		entity work.max_layer
 			Generic Map (input_size => outputs)
 			Port Map (Din => m, Dout=>Dout);
 	end generate;
-	
-	
-	Last_Layer: 
+
+
+	Last_Layer:
 	if input_size = 2 generate
 		Dout <= m(0);
 	end generate;
