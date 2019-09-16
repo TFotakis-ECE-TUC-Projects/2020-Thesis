353a09c163e61401ac21060987388622c6a8c80e
TFotakis <fotakistzanis@gmail.com>
2019-09-17 01:34:51 +0300

    Created multiplier_unit.vhd

---
+++ Projects/Vivado/AlexNet/AlexNet.srcs/sources_1/new/multiplier_unit.vhd
@@ -0,0 +1,41 @@
+library ieee;
+use ieee.std_logic_1164.all;
+-- use ieee.math_real.all;
+-- use ieee.numeric_std.all;
+use ieee.std_logic_signed.all;
+use work.bus_array_pkg.all;
+
+
+entity multiplier_unit is
+	Generic (
+		use_float: boolean := true;
+		output_width: positive := 32
+	);
+	Port (
+		Din: in bus_array (1 downto 0);
+		Dout: out std_logic_vector (output_width - 1 downto 0)
+	);
+end multiplier_unit;
+
+
+architecture Structural of multiplier_unit is
+begin
+	int:
+	if not use_float generate
+		Dout <= Din(0) * Din(1);
+	end generate;
+
+	float:
+	if use_float generate
+		ip_float_multiplier_unit:
+		entity work.float_multiplier_unit
+			Port Map(
+				s_axis_a_tvalid => '1',
+				s_axis_a_tdata => Din(0),
+				s_axis_b_tvalid => '1',
+				s_axis_b_tdata => Din(1),
+				-- m_axis_result_tvalid => tmp_valid,
+				m_axis_result_tdata => Dout
+			);
+	end generate;
+end Structural;
