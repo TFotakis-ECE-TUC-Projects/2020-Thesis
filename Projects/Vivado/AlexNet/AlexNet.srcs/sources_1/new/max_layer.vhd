5b2111047e99ba33985eff04cff3b260e69184be
TFotakis <fotakistzanis@gmail.com>
2019-09-17 03:25:36 +0300

    Added use_float functionality on max_layer.vhd

--- Projects/Vivado/AlexNet/AlexNet.srcs/sources_1/new/max_layer.vhd
+++ Projects/Vivado/AlexNet/AlexNet.srcs/sources_1/new/max_layer.vhd
@@ -5,7 +5,10 @@ use work.bus_array_pkg.all;
 
 
 entity max_layer is
-	generic	(input_size: positive := 9);
+	Generic	(
+		use_float: boolean := true;
+		input_size: positive := 9
+	);
 	Port (
 		Din: in bus_array(input_size - 1 downto 0);
 		Dout: out STD_LOGIC_VECTOR (31 downto 0)
@@ -23,28 +26,31 @@ begin
 	for i in 0 to modules - 1 generate
 		max_x:
 		entity work.max
-			Port Map(
+			Generic Map (use_float => use_float)
+			Port Map (
 				In0 => Din(i * 2),
 				In1 => Din(i * 2 + 1),
 				Dout => m(i)
 			);
 	end generate;
 
-
 	Odd_Input:
 	if input_size mod 2 = 1 generate
 		m(outputs - 1) <= Din(input_size - 1);
 	end generate;
 
-
 	Next_Layer:
 	if input_size /= 2 generate
 		next_max_layer:
 		entity work.max_layer
-			Generic Map (input_size => outputs)
-			Port Map (Din => m, Dout=>Dout);
+			Generic Map (
+				use_float => use_float,
+				input_size => outputs
+			)
+			Port Map (
+				Din => m, Dout=>Dout
+			);
 	end generate;
-
 
 	Last_Layer:
 	if input_size = 2 generate
