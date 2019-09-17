f22fc26e72cb394175db7134195348649e64113f
TFotakis <fotakistzanis@gmail.com>
2019-09-17 03:22:48 +0300

    Added on tb_maxpool.vhd use_float functionality

--- Projects/Vivado/AlexNet/AlexNet.srcs/sim_1/new/tb_maxpool.vhd
+++ Projects/Vivado/AlexNet/AlexNet.srcs/sim_1/new/tb_maxpool.vhd
@@ -9,29 +9,40 @@ entity tb_maxpool is
 end tb_maxpool;
 
 architecture tb of tb_maxpool is
+	constant use_float: boolean := true;
 	constant input_size: positive := 9;
 
 	signal Din: bus_array(input_size - 1 downto 0);
 	signal Dout: std_logic_vector (31 downto 0);
 begin
-	dut:
-	entity work.max_layer
-		Generic Map (input_size => input_size)
-		Port Map (
-			Din => Din,
-			Dout => Dout
-		);
+	 dut:
+	 entity work.max_layer
+	 	Generic Map (
+	 		use_float => use_float,
+	 		input_size => input_size
+	 	)
+	 	Port Map (
+	 		Din => Din,
+	 		Dout => Dout
+	 	);
 
 	stimuli:
-	process begin
+	process
+	begin
     	for j in 0 to 10 loop
 			for i in 0 to (input_size - 1) / 2 loop
-				-- Din(i) <= std_logic_vector(to_signed(i + j * input_size, 32));
-				Din(i) <= real_to_flt(0.1 * real(i + j * input_size + 3), normal, 32, 24);
+				if not use_float then
+					Din(i) <= std_logic_vector(to_signed(i + j * input_size, 32));
+				else
+					Din(i) <= real_to_flt(0.1 * real(i + j * input_size + 3), normal, 32, 24);
+				end if;
 			end loop;
 			for i in (input_size - 1) / 2 + 1 to input_size - 1 loop
-				-- Din(i) <= std_logic_vector(to_signed(i + j * input_size, 32));
-				Din(i) <= real_to_flt(-0.1 * real(i + j * input_size + 3), normal, 32, 24);
+				if not use_float then
+					Din(i) <= std_logic_vector(to_signed(-1 * (i + j * input_size), 32));
+				else
+					Din(i) <= real_to_flt(-0.1 * real(i + j * input_size + 3), normal, 32, 24);
+				end if;
 			end loop;
 			wait for 100 ns;
 		end loop;
