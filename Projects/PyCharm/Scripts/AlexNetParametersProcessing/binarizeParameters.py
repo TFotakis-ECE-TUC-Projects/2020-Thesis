68bbe5bf371c33bb6a1c1b66a34c9ec360d459f4
TFotakis <fotakistzanis@gmail.com>
2019-10-29 17:38:46 +0200

    Made binarizeParameters.py script generate a binary parameters file

--- Projects/PyCharm/Scripts/AlexNetParametersProcessing/binarizeParameters.py
+++ Projects/PyCharm/Scripts/AlexNetParametersProcessing/binarizeParameters.py
@@ -1,36 +1,18 @@
 import torch.utils.model_zoo as model_zoo
-import numpy
-
-
-def joinNumArray(arr):
-	if type(arr) is numpy.ndarray:
-		return ' '.join(f"{el:.30f}" for el in arr)
-	return ' '.join(str(el) for el in arr)
-
-
-def joinMatrix(arr):
-	dim1 = 1
-	for dim in arr.shape[:-1]:
-		dim1 *= dim
-	dim2 = arr.shape[-1]
-
-	return '\n'.join(joinNumArray(el) for el in state_dict[k].reshape((dim1, dim2)))
-
+import numpy as np
 
 model_urls = {'alexnet': 'https://download.pytorch.org/models/alexnet-owt-4df8aa71.pth'}
 state_dict = model_zoo.load_url(model_urls['alexnet'])
 state_dict = {key: state_dict[key].detach().numpy() for key in state_dict}
 
-string = str(len(state_dict.keys())) + '\n'
-for k in state_dict.keys():
-	string += str(len(state_dict[k].shape)) + ' ' + joinNumArray(state_dict[k].shape) + '\n'
-	if len(state_dict[k].shape) == 1:
-		string += joinNumArray(state_dict[k]) + '\n'
-	else:
-		string += joinMatrix(state_dict[k]) + '\n'
-
-
-with open("binaryParametersBig.txt", 'w') as fp:
-	fp.write(string)
-
+with open("output/alexnetParams", 'wb') as fp:
+	arr = np.array([len(state_dict.keys())], dtype='int32')
+	arr.tofile(fp)
+	for k in state_dict.keys():
+		arr = np.array([len(state_dict[k].shape)] + list(state_dict[k].shape), dtype='int32')
+		arr.tofile(fp)
+		arr = state_dict[k].flatten()
+		arr.tofile(fp)
+fp.close()
 print("Done")
+exit()
