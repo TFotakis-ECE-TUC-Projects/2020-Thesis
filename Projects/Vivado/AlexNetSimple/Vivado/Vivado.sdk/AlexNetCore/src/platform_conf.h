0cc6034c1692f0cd11bb12e7652683e54cc78efb
TFotakis <fotakistzanis@gmail.com>
2020-01-31 04:56:04 +0200

    Updated AlexNetSimple sdk

--- Projects/Vivado/AlexNetSimple/Vivado/Vivado.sdk/AlexNetCore/src/platform_conf.h
+++ Projects/Vivado/AlexNetSimple/Vivado/Vivado.sdk/AlexNetCore/src/platform_conf.h
@@ -12,7 +12,7 @@
 
 #define BASE_ADDR 0x50000000
 
-typedef double matrix_t;
+typedef float matrix_t;
 
 typedef enum {
 	Conv,
@@ -45,13 +45,195 @@ typedef struct {
 	u32 doReLU;
 } LayerConf;
 
-XConv_core *Conv_core_list[CONV_CORES_NUM];
-XConv_core_Config *Conv_core_cfg_list[CONV_CORES_NUM];
+typedef struct {
+	unsigned int InstanceId;
+	void *InstancePtr;
+} Core;
 
-XMaxpool_core Maxpool_core_list[MAXPOOL_CORES_NUM];
-XMaxpool_core_Config *Maxpool_core_cfg_list[MAXPOOL_CORES_NUM];
+Core *Conv_core_list[CONV_CORES_NUM];
 
-XLinear_core Linear_core_list[LINEAR_CORES_NUM];
-XLinear_core_Config *Linear_core_cfg_list[LINEAR_CORES_NUM];
+Core *Maxpool_core_list[MAXPOOL_CORES_NUM];
+
+Core *Linear_core_list[LINEAR_CORES_NUM];
+
+#if CONV_CORES_NUM == 1
+const unsigned int Conv_core_device_ids[CONV_CORES_NUM] = {
+	XPAR_XCONV_CORE_0_DEVICE_ID,
+};
+const unsigned int Conv_core_interrupt_ids[CONV_CORES_NUM] = {
+	XPAR_FABRIC_CONV_CORE_0_INTERRUPT_INTR,
+};
+volatile static int Conv_core_result_avail[CONV_CORES_NUM] = {
+	0,
+};
+#elif CONV_CORES_NUM == 2
+const unsigned int Conv_core_device_ids[CONV_CORES_NUM] = {
+	XPAR_XCONV_CORE_0_DEVICE_ID,
+	XPAR_XCONV_CORE_1_DEVICE_ID,
+};
+const unsigned int Conv_core_interrupt_ids[CONV_CORES_NUM] = {
+	XPAR_FABRIC_CONV_CORE_0_INTERRUPT_INTR,
+	XPAR_FABRIC_CONV_CORE_1_INTERRUPT_INTR,
+};
+volatile static int Conv_core_result_avail[CONV_CORES_NUM] = {
+	0,
+	0,
+};
+#elif CONV_CORES_NUM == 3
+const unsigned int Conv_core_device_ids[CONV_CORES_NUM] = {
+	XPAR_XCONV_CORE_0_DEVICE_ID,
+	XPAR_XCONV_CORE_1_DEVICE_ID,
+	XPAR_XCONV_CORE_2_DEVICE_ID,
+};
+const unsigned int Conv_core_interrupt_ids[CONV_CORES_NUM] = {
+	XPAR_FABRIC_CONV_CORE_0_INTERRUPT_INTR,
+	XPAR_FABRIC_CONV_CORE_1_INTERRUPT_INTR,
+	XPAR_FABRIC_CONV_CORE_2_INTERRUPT_INTR,
+};
+volatile static int Conv_core_result_avail[CONV_CORES_NUM] = {
+	0,
+	0,
+	0,
+};
+#elif CONV_CORES_NUM == 4
+const unsigned int Conv_core_device_ids[CONV_CORES_NUM] = {
+	XPAR_XCONV_CORE_0_DEVICE_ID,
+	XPAR_XCONV_CORE_1_DEVICE_ID,
+	XPAR_XCONV_CORE_2_DEVICE_ID,
+	XPAR_XCONV_CORE_3_DEVICE_ID,
+};
+const unsigned int Conv_core_interrupt_ids[CONV_CORES_NUM] = {
+	XPAR_FABRIC_CONV_CORE_0_INTERRUPT_INTR,
+	XPAR_FABRIC_CONV_CORE_1_INTERRUPT_INTR,
+	XPAR_FABRIC_CONV_CORE_2_INTERRUPT_INTR,
+	XPAR_FABRIC_CONV_CORE_3_INTERRUPT_INTR,
+};
+volatile static int Conv_core_result_avail[CONV_CORES_NUM] = {
+	0,
+	0,
+	0,
+	0,
+};
+#endif
+
+#if MAXPOOL_CORES_NUM == 1
+const unsigned int Maxpool_core_device_ids[MAXPOOL_CORES_NUM] = {
+	XPAR_XMAXPOOL_CORE_0_DEVICE_ID,
+};
+const unsigned int Maxpool_core_interrupt_ids[MAXPOOL_CORES_NUM] = {
+	XPAR_FABRIC_MAXPOOL_CORE_0_INTERRUPT_INTR,
+};
+volatile static int Maxpool_core_result_avail[CONV_CORES_NUM] = {
+	0,
+};
+#elif MAXPOOL_CORES_NUM == 2
+const unsigned int Maxpool_core_device_ids[MAXPOOL_CORES_NUM] = {
+	XPAR_XMAXPOOL_CORE_0_DEVICE_ID,
+	XPAR_XMAXPOOL_CORE_1_DEVICE_ID,
+};
+const unsigned int Maxpool_core_interrupt_ids[MAXPOOL_CORES_NUM] = {
+	XPAR_FABRIC_MAXPOOL_CORE_0_INTERRUPT_INTR,
+	XPAR_FABRIC_MAXPOOL_CORE_1_INTERRUPT_INTR,
+};
+volatile static int Maxpool_core_result_avail[CONV_CORES_NUM] = {
+	0,
+	0,
+};
+#elif MAXPOOL_CORES_NUM == 3
+const unsigned int Maxpool_core_device_ids[MAXPOOL_CORES_NUM] = {
+	XPAR_XMAXPOOL_CORE_0_DEVICE_ID,
+	XPAR_XMAXPOOL_CORE_1_DEVICE_ID,
+	XPAR_XMAXPOOL_CORE_2_DEVICE_ID,
+};
+const unsigned int Maxpool_core_interrupt_ids[MAXPOOL_CORES_NUM] = {
+	XPAR_FABRIC_MAXPOOL_CORE_0_INTERRUPT_INTR,
+	XPAR_FABRIC_MAXPOOL_CORE_1_INTERRUPT_INTR,
+	XPAR_FABRIC_MAXPOOL_CORE_2_INTERRUPT_INTR,
+};
+volatile static int Maxpool_core_result_avail[CONV_CORES_NUM] = {
+	0,
+	0,
+	0,
+};
+#elif MAXPOOL_CORES_NUM == 4
+const unsigned int Maxpool_core_device_ids[MAXPOOL_CORES_NUM] = {
+	XPAR_XMAXPOOL_CORE_0_DEVICE_ID,
+	XPAR_XMAXPOOL_CORE_1_DEVICE_ID,
+	XPAR_XMAXPOOL_CORE_2_DEVICE_ID,
+	XPAR_XMAXPOOL_CORE_3_DEVICE_ID,
+};
+const unsigned int Maxpool_core_interrupt_ids[MAXPOOL_CORES_NUM] = {
+	XPAR_FABRIC_MAXPOOL_CORE_0_INTERRUPT_INTR,
+	XPAR_FABRIC_MAXPOOL_CORE_1_INTERRUPT_INTR,
+	XPAR_FABRIC_MAXPOOL_CORE_2_INTERRUPT_INTR,
+	XPAR_FABRIC_MAXPOOL_CORE_3_INTERRUPT_INTR,
+};
+volatile static int Maxpool_core_result_avail[CONV_CORES_NUM] = {
+	0,
+	0,
+	0,
+	0,
+};
+#endif
+
+#if LINEAR_CORES_NUM == 1
+const unsigned int Linear_core_device_ids[LINEAR_CORES_NUM] = {
+	XPAR_XLINEAR_CORE_0_DEVICE_ID,
+};
+const unsigned int Linear_core_interrupt_ids[LINEAR_CORES_NUM] = {
+	XPAR_FABRIC_LINEAR_CORE_0_INTERRUPT_INTR,
+};
+volatile static int Linear_core_result_avail[CONV_CORES_NUM] = {
+	0,
+};
+#elif LINEAR_CORES_NUM == 2
+const unsigned int Linear_core_device_ids[LINEAR_CORES_NUM] = {
+	XPAR_XLINEAR_CORE_0_DEVICE_ID,
+	XPAR_XLINEAR_CORE_1_DEVICE_ID,
+};
+const unsigned int Linear_core_interrupt_ids[LINEAR_CORES_NUM] = {
+	XPAR_FABRIC_LINEAR_CORE_0_INTERRUPT_INTR,
+	XPAR_FABRIC_LINEAR_CORE_1_INTERRUPT_INTR,
+};
+volatile static int Linear_core_result_avail[CONV_CORES_NUM] = {
+	0,
+	0,
+};
+#elif LINEAR_CORES_NUM == 3
+const unsigned int Linear_core_device_ids[LINEAR_CORES_NUM] = {
+	XPAR_XLINEAR_CORE_0_DEVICE_ID,
+	XPAR_XLINEAR_CORE_1_DEVICE_ID,
+	XPAR_XLINEAR_CORE_2_DEVICE_ID,
+};
+const unsigned int Linear_core_interrupt_ids[LINEAR_CORES_NUM] = {
+	XPAR_FABRIC_LINEAR_CORE_0_INTERRUPT_INTR,
+	XPAR_FABRIC_LINEAR_CORE_1_INTERRUPT_INTR,
+	XPAR_FABRIC_LINEAR_CORE_2_INTERRUPT_INTR,
+};
+volatile static int Linear_core_result_avail[CONV_CORES_NUM] = {
+	0,
+	0,
+	0,
+};
+#elif LINEAR_CORES_NUM == 4
+const unsigned int Linear_core_device_ids[LINEAR_CORES_NUM] = {
+	XPAR_XLINEAR_CORE_0_DEVICE_ID,
+	XPAR_XLINEAR_CORE_1_DEVICE_ID,
+	XPAR_XLINEAR_CORE_2_DEVICE_ID,
+	XPAR_XLINEAR_CORE_3_DEVICE_ID,
+};
+const unsigned int Linear_core_interrupt_ids[LINEAR_CORES_NUM] = {
+	XPAR_FABRIC_LINEAR_CORE_0_INTERRUPT_INTR,
+	XPAR_FABRIC_LINEAR_CORE_1_INTERRUPT_INTR,
+	XPAR_FABRIC_LINEAR_CORE_2_INTERRUPT_INTR,
+	XPAR_FABRIC_LINEAR_CORE_3_INTERRUPT_INTR,
+};
+volatile static int Linear_core_result_avail[CONV_CORES_NUM] = {
+	0,
+	0,
+	0,
+	0,
+};
+#endif
 
 #endif /* SRC_PLATFORM_CONF_H_ */
