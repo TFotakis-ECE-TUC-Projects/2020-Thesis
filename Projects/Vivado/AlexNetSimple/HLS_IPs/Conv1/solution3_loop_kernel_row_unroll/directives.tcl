38cb29fd2fbc34a2b650c5ef79e932800d0ac380
TFotakis <fotakistzanis@gmail.com>
2020-02-16 23:51:07 +0200

    Removed AlexNetSimple from Vivado projects

--- Projects/Vivado/AlexNetSimple/HLS_IPs/Conv1/solution3_loop_kernel_row_unroll/directives.tcl
+++
@@ -1,7 +0,0 @@
-############################################################
-## This file is generated automatically by Vivado HLS.
-## Please DO NOT edit it.
-## Copyright (C) 1986-2019 Xilinx, Inc. All Rights Reserved.
-############################################################
-set_directive_unroll "Conv_Core/Loop_kernel_pixel"
-set_directive_unroll "Conv_Core/Loop_kernel_row"
--- Projects/Vivado/AlexNetSimple/HLS_IPs/Conv1/solution5_loop_kernel_pipeline/directives.tcl
+++
@@ -1,8 +0,0 @@
-############################################################
-## This file is generated automatically by Vivado HLS.
-## Please DO NOT edit it.
-## Copyright (C) 1986-2019 Xilinx, Inc. All Rights Reserved.
-############################################################
-set_directive_unroll "Conv_Core/Loop_kernel_pixel"
-set_directive_unroll "Conv_Core/Loop_kernel_row"
-set_directive_pipeline "Conv_Core/Loop_kernel"
