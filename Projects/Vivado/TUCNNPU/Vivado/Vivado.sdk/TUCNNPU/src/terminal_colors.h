1483fa8709c7beda04bf5b88222b875281d782ba
TFotakis <fotakistzanis@gmail.com>
2020-02-17 17:53:36 +0200

    Updated terminal_colors.h on TUCNNPU

--- Projects/Vivado/TUCNNPU/Vivado/Vivado.sdk/TUCNNPU/src/terminal_colors.h
+++ Projects/Vivado/TUCNNPU/Vivado/Vivado.sdk/TUCNNPU/src/terminal_colors.h
@@ -1,6 +1,4 @@
-/*
- * terminal_colors.h
- *
+/**
  * Contains definitions of terminal color characters.
  *
  * Use by printing the color character before the string to be colored.
@@ -14,36 +12,13 @@
 #ifndef SRC_TERMINAL_COLORS_H_
 #define SRC_TERMINAL_COLORS_H_
 
-/** Normal */
-#define KNRM  "\x1B[0m"
+#define KNRM "\x1B[0m"  /** Normal */
+#define KRED "\x1B[31m" /** Red */
+#define KGRN "\x1B[32m" /** Green */
+#define KYEL "\x1B[33m" /** Yellow */
+#define KBLU "\x1B[34m" /** Blue */
+#define KMAG "\x1B[35m" /** Magenta */
+#define KCYN "\x1B[36m" /** Cyan */
+#define KWHT "\x1B[37m" /** White */
 
-
-/** Red */
-#define KRED  "\x1B[31m"
-
-
-/** Green */
-#define KGRN  "\x1B[32m"
-
-
-/** Yellow */
-#define KYEL  "\x1B[33m"
-
-
-/** Blue */
-#define KBLU  "\x1B[34m"
-
-
-/** Magenta */
-#define KMAG  "\x1B[35m"
-
-
-/** Cyan */
-#define KCYN  "\x1B[36m"
-
-
-/** White */
-#define KWHT  "\x1B[37m"
-
-
-#endif /* SRC_TERMINAL_COLORS_H_ */
+#endif
