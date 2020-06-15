59a17c9dfc68ac863d09f4b44222f9e50dfcfad2
TFotakis <fotakistzanis@gmail.com>
2020-06-16 00:55:50 +0300

    Utilize fixptbestexp function in QuantizeFixed.m

--- Projects/Matlab/CNN-Tools/Functions/QuantizeFixed.m
+++ Projects/Matlab/CNN-Tools/Functions/QuantizeFixed.m
@@ -1,14 +1,19 @@
 function x = QuantizeFixed(x, bitWidth)
 x = double(x);
-s = zeros(1, bitWidth);
-for r=1:bitWidth
-	xFixed = num2fixpt(x, fixdt(1, bitWidth, r), 'Nearest');
-	xAbs = abs(x - xFixed);
-	s(r) = sum(xAbs(:)) / numel(x);
-end
 
-[m, rep] = min(s);
+% s = zeros(1, bitWidth);
+% for r=1:bitWidth
+% % for r=1:32
+% 	xFixed = num2fixpt(x, fixdt(1, bitWidth, r), 'Nearest');
+% 	xAbs = abs(x - xFixed) .^ 1;
+% 	s(r) = sum(xAbs(:)) / numel(x);
+% end
+% [m, rep] = min(s);
+
+rep = -fixptbestexp(x(:), bitWidth, 1);
+rep = int32(rep);
+m=0;
 
 x = num2fixpt(x, fixdt(1, bitWidth, rep));
-fprintf("Fixed %u.%u, accuracy: %f\n", bitWidth, rep, m);
+fprintf("Fixed %u.%u, Error rate: %f\n", bitWidth, rep, m);
 end
